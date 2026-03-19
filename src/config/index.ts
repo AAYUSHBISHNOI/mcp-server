7c254fa144b57bc217a2b7385090ae920c53ef53
AAYUSHBISHNOI <aayushkaswan79@gmail.com>
2026-03-19 16:36:57 +0530

    git-file-added

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,4 +4,7 @@ dotenv.config();
 export const N8N_BASE_URL =
     process.env.N8N_BASE_URL || "https://authentic-surprise-production.up.railway.app";
 
+
+
+
 export const PORT = process.env.PORT || 3001;
