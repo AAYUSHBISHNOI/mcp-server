1371deb659ddf690bbc7eaf39775bbf8567e3428
AAYUSHBISHNOI <aayushkaswan79@gmail.com>
2026-04-08 17:07:01 +0530

    new-keys-added

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const N8N_BASE_URL =
-    process.env.N8N_BASE_URL || "https://primary-production-564bc.up.railway.app";
+    process.env.N8N_BASE_URL || "https://primary-production-f3a1a.up.railway.app;
 
 export const PORT = process.env.PORT || 3001;
